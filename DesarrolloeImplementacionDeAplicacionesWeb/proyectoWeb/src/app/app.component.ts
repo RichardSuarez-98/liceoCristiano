2bf4aff3d7a19fe0c6f6b984758e877bafde0e48
Richard <richard.suarezjacome2021@gmail.com>
2024-12-23 21:57:29 -0500

    init

--- DesarrolloeImplementacionDeAplicacionesWeb/proyectoWeb/src/app/app.component.ts
+++ DesarrolloeImplementacionDeAplicacionesWeb/proyectoWeb/src/app/app.component.ts
@@ -4,11 +4,10 @@ import { RouterOutlet } from '@angular/router';
 import { ListadoPeliculasComponent } from "./peliculas/listado-peliculas/listado-peliculas.component";
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet, CommonModule, NgOptimizedImage, ListadoPeliculasComponent],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+    selector: 'app-root',
+    imports: [RouterOutlet, CommonModule, NgOptimizedImage, ListadoPeliculasComponent],
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.css'
 })
 export class AppComponent implements OnInit{
   title = 'proyectoWeb';
--- DesarrolloeImplementacionDeAplicacionesWeb/proyectoWeb/src/app/peliculas/listado-peliculas/listado-peliculas.component.ts
+++ DesarrolloeImplementacionDeAplicacionesWeb/proyectoWeb/src/app/peliculas/listado-peliculas/listado-peliculas.component.ts
@@ -1,12 +1,14 @@
 import { CommonModule } from '@angular/common';
 import { Component, Input } from '@angular/core';
+import { ListadoGenericoComponent } from '../../compartidos/componentes/listado-generico/listado-generico.component';
+import {MatIconModule} from '@angular/material/icon';
+import {MatButtonModule} from '@angular/material/button';
 
 @Component({
-  selector: 'app-listado-peliculas',
-  standalone: true,
-  imports: [CommonModule],
-  templateUrl: './listado-peliculas.component.html',
-  styleUrl: './listado-peliculas.component.css'
+    selector: 'app-listado-peliculas',
+    imports: [CommonModule,MatButtonModule, ListadoGenericoComponent,MatIconModule,MatButtonModule],
+    templateUrl: './listado-peliculas.component.html',
+    styleUrl: './listado-peliculas.component.css'
 })
 export class ListadoPeliculasComponent {
 
