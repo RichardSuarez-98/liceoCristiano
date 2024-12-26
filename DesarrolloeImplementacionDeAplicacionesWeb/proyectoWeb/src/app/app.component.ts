20582050bbf9b1db1ec6dd739c82a4c05c9b9ad5
Richard <richard.suarezjacome2021@gmail.com>
2024-12-25 20:09:47 -0500

    rating

--- DesarrolloeImplementacionDeAplicacionesWeb/proyectoWeb/src/app/app.component.ts
+++ DesarrolloeImplementacionDeAplicacionesWeb/proyectoWeb/src/app/app.component.ts
@@ -2,10 +2,12 @@ import { CommonModule, NgOptimizedImage } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { ListadoPeliculasComponent } from "./peliculas/listado-peliculas/listado-peliculas.component";
+import { MenuComponent } from "./compartidos/componentes/menu/menu.component";
+import { RatingComponent } from "./compartidos/componentes/rating/rating.component";
 
 @Component({
     selector: 'app-root',
-    imports: [RouterOutlet, CommonModule, NgOptimizedImage, ListadoPeliculasComponent],
+    imports: [CommonModule, ListadoPeliculasComponent, MenuComponent, RatingComponent],
     templateUrl: './app.component.html',
     styleUrl: './app.component.css'
 })
@@ -63,6 +65,10 @@ export class AppComponent implements OnInit{
 
    }
 
+
+   procesarVoto(voto:number){
+    alert(`Calificación otortgados: ${voto}`);
+   }
 
 
 
