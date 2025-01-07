3cab6ec3fff4619641f779d8ad46cc72a3e25731
Richard <richard.suarezjacome2021@gmail.com>
2025-01-07 09:33:20 -0500

    routing in angular

--- DesarrolloeImplementacionDeAplicacionesWeb/proyectoWeb/src/app/peliculas/listado-peliculas/listado-peliculas.component.ts
+++ DesarrolloeImplementacionDeAplicacionesWeb/proyectoWeb/src/app/peliculas/listado-peliculas/listado-peliculas.component.ts
@@ -14,18 +14,18 @@ export class ListadoPeliculasComponent {
 
   @Input({required: true}) listadPeliculas: any=[];
 
-  agregarPelicula(){
+  /*agregarPelicula(){
     this.listadPeliculas.push({
       titulo: 'Piratas del caribe 8',
       fechaLanzamiento: new Date('2029-03-02'),
       precio: 147.62,
       poster: ''
     });
-  }
+  }*/
 
-  removerPelicula(pelicula: any){
+  /*removerPelicula(pelicula: any){
     const indice = this.listadPeliculas.findIndex((peliculaActual:any)=> peliculaActual.titulo === pelicula.titulo);
     this.listadPeliculas.splice(indice,1);
-  }
+  }*/
 
 }
